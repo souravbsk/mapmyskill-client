8543ebc03457baada9e103e57d60f37b0bf3f11d
souravbsk <souravbsk01@gmail.com>
2023-12-08 12:24:50 +0600

    map-my-skill

--- src/Pages/UserPages/Home/Home.jsx
+++ src/Pages/UserPages/Home/Home.jsx
@@ -1,35 +1,29 @@
-import React from 'react';
-import Banner from './Banner/Banner';
-import Request from './Request/Request';
-import HowItWork from './HowItWork/HowItWork';
-import Banner2 from './Banner2/Banner2';
-import NewBanner from './newBanner/newBanner';
-
-import Services from './Services/Services';
-import Testimonials from './Testimonials/Testimonials';
-import Tutors from './Tutors/Tutors';
-import Stats from './Stats/Stats';
-import Faq from './Faq/Faq';
-
+import React from "react";
+import Request from "./Request/Request";
+import HowItWork from "./HowItWork/HowItWork";
+import NewBanner from "./newBanner/newBanner";
+import Services from "./Services/Services";
+import Testimonials from "./Testimonials/Testimonials";
+import Tutors from "./Tutors/Tutors";
+import Stats from "./Stats/Stats";
+import Faq from "./Faq/Faq";
 
 const Home = () => {
-    return (
-        <div>
-              <NewBanner></NewBanner>
-            <Request></Request>
-            <HowItWork></HowItWork>
+  return (
+    <div>
+      <NewBanner></NewBanner>
+      <Request></Request>
+      <HowItWork></HowItWork>
 
-         <div className='lg:px-[100px] px-2'>
-         <Services></Services>
-            <Testimonials></Testimonials>
-            <Faq></Faq>
-           
-            <Tutors></Tutors>
-            <Stats></Stats>
-         </div>
-
-        </div>
-    );
+      <div className="lg:px-[100px] px-2">
+        <Services></Services>
+        <Testimonials></Testimonials>
+        <Faq></Faq>
+        <Tutors></Tutors>
+        <Stats></Stats>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
